cfd617fa04bf04b42da2b3661affb7eadd94c7bc
Philip Newborough <corenominal@corenominal.org>
2026-03-11 11:42:54 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# notifications.philipnewborough.co.uk
---
+++ app/Config/Routes.php
@@ -0,0 +1,32 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+// Admin routes
+$routes->get('/admin', 'Admin\Home::index');
+
+// API routes
+$routes->match(['get', 'options'], '/api/test/ping', 'Api\Test::ping');
+
+// Command line routes
+$routes->cli('cli/test/index/(:segment)', 'CLI\Test::index/$1');
+$routes->cli('cli/test/count', 'CLI\Test::count');
+
+// Logout route
+$routes->get('/logout', 'Auth::logout');
+
+// Unauthorised route
+$routes->get('/unauthorised', 'Unauthorised::index');
+
+// Custom 404 route
+$routes->set404Override('App\Controllers\Errors::show404');
+
+// Debug routes
+$routes->get('/debug', 'Debug\Home::index');
+$routes->get('/debug/(:segment)', 'Debug\Rerouter::reroute/$1');
+$routes->get('/debug/(:segment)/(:segment)', 'Debug\Rerouter::reroute/$1/$2');
