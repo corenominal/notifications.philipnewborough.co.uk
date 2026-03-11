83b4ace4bcd4c5e14e2f87ca36f224a511cf21d7
Philip Newborough <corenominal@corenominal.org>
2026-03-11 21:26:39 +0000

    Updated routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,6 +13,18 @@ $routes->get('/admin', 'Admin\Home::index');
 // API routes
 $routes->match(['get', 'options'], '/api/test/ping', 'Api\Test::ping');
 
+$routes->match(['get', 'options'], '/api/notifications/(:segment)', 'Api\Notifications::index/$1');
+$routes->match(['get', 'options'], '/api/notifications/(:segment)/(:num)', 'Api\Notifications::index/$1/$2');
+
+$routes->match(['post', 'options'], '/api/notification', 'Api\Notification::insert');
+$routes->match(['post', 'options'], '/api/notification/clear', 'Api\Notification::clear');
+$routes->match(['post', 'options'], '/api/notification/clearall', 'Api\Notification::clearall');
+$routes->match(['post', 'options'], '/api/notification/read', 'Api\Notification::read');
+$routes->match(['post', 'options'], '/api/notification/readall', 'Api\Notification::readall');
+
+// TODO: Add route for subscription insertion
+// $routes->match(['post', 'options'], '/api/subscription', 'Api\Subscription::insert');
+
 // Command line routes
 $routes->cli('cli/test/index/(:segment)', 'CLI\Test::index/$1');
 $routes->cli('cli/test/count', 'CLI\Test::count');
